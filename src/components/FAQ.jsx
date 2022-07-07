ade8a29c791e8d2fb934396cd90b78bcc0535590
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-07 17:03:16 +0000

    setup icons for FAQs

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import styled from "styled-components";
+import { IoIosArrowForward } from "react-icons/io";
+import { MdTimelapse } from "react-icons/md";
+import { IoMdCash } from "react-icons/io";
+import { AiFillCalendar } from "react-icons/ai";
+import { cardStyle } from "./ReusableStyles";
 const FAQ = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const faqs = [
+    {
+      icon: <AiFillCalendar />,
+      text: "How to manage time and get good marks for a trips",
+    },
+    {
+      icon: <MdTimelapse />,
+      text: "How to regulate transactions over time",
+    },
+    {
+      icon: <AiFillCalendar />,
+      text: "Withdrawing money through an ATM",
+    },
+  ];
+  return <div></div>;
+};
 
-export default FAQ
+export default FAQ;
