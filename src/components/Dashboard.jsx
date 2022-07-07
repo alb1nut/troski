e507e2000e93fc18ee769fcb5e394dc362d8e3ab
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-07 15:39:34 +0000

    setup grid layout for dashboard section

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Navbar from "./Navbar";
+import FAQ from "./FAQ";
+import Analytics from "./Analytics";
+import Profile from "./Profile";
+import Transfers from "./Transfers";
+import Earnings from "./Earnings";
 
 const Dashboard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Section>
+      <Navbar />
+      <div className="grid">
+        <div className="row_one">
+          <Analytics />
+          <FAQ />
+        </div>
+        <div className="row_two">
+          <Earnings />
+          <Transfers />
+          <Profile />
+        </div>
+      </div>
+    </Section>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
+
+const Section = styled.section``;
