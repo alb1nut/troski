c8dbd40851e7051b504af1d41b9684b8aa439fd6
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-08 04:31:51 +0000

    completed styling of profiles component in dashboard

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -37,4 +37,47 @@ const Profile = () => {
 
 export default Profile;
 
-const Section = styled.section``;
+const Section = styled.section`
+  ${cardStyle}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  .image {
+    max-height: 10rem;
+    overflow: hidden;
+    border-radius: 20rem;
+    img {
+      height: 10rem;
+      width: 10rem;
+      object-fit: cover;
+      border-radius: 20rem;
+      transition: 0.5s ease-in-out;
+    }
+    &:hover {
+      img {
+        transform: scale(1.1);
+      }
+    }
+  }
+
+  .title {
+    text-align: center;
+    h2,
+    h5 {
+      color: #41ff07;
+      font-family: "Permanent Marker", cursive;
+      letter-spacing: 0.3rem;
+    }
+    h5 {
+      letter-spacing: 0.2rem;
+    }
+  }
+  .info {
+    display: flex;
+    gap: 1rem;
+    .container {
+      text-align: center;
+    }
+  }
+`;
