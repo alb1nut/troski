59193554724aa23230f70829689dfed839fa9ab0
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-08 02:15:00 +0000

    completed styling of faqs component in dashboard

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -42,4 +42,38 @@ const FAQ = () => {
 
 export default FAQ;
 
-const Section = styled.section``;
+const Section = styled.section`
+  ${cardStyle}
+  .title {
+    h2 {
+      color: #41ff07;
+      font-family: "Permanent Marker", cursive;
+      letter-spacing: 0.3rem;
+    }
+  }
+  .faqs {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-top: 1rem;
+    .faq {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      cursor: pointer;
+      .info {
+        display: flex;
+        gap: 1rem;
+        align-items: center;
+      }
+      svg {
+        font-size: 1.4rem;
+      }
+      &:nth-of-type(2) {
+        border-top: 0.01rem solid #6c6e6e;
+        border-bottom: 0.01rem solid #6c6e6e;
+        padding: 0.8rem 0;
+      }
+    }
+  }
+`;
