4979c6e710972872b665f8f929124a8480b6ef0c
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-08 02:54:19 +0000

    built structure for transfers section

--- src/components/Transfers.jsx
+++ src/components/Transfers.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
+import { HiArrowNarrowRight } from "react-icons/hi";
+import avatarImage from "../assets/avatarImage.jpeg";
+import { cardStyle } from "./ReusableStyles";
 const Transfers = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const transactions = [
+    {
+      image: avatarImage,
+      name: "From Daniel Owusu",
+      time: "Today, 16:35",
+      amount: "+Ghc50",
+    },
+    {
+      image: avatarImage,
+      name: "To Absa Bank",
+      time: "Today, 08:35",
+      amount: "-Ghc400",
+    },
+    {
+      image: avatarImage,
+      name: "From Kofi Dadzi",
+      time: "Yesterday, 12:35",
+      amount: "+Ghc200",
+    },
+  ];
+  return <Section></Section>;
+};
 
-export default Transfers
+export default Transfers;
+
+const Section = styled.section``;
